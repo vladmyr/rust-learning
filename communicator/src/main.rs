136a1358c54decad25f170c8df602ab1851c60b2
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-11-08 21:39:30 +0100

    bugfix

--- communicator/src/main.rs
+++ communicator/src/main.rs
@@ -2,6 +2,6 @@ extern crate communicator;
 
 fn main() {
   communicator::network::connect();
-  communicator::server::connect();
+  communicator::network::server::connect();
   communicator::client::connect();
 }
