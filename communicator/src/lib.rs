eee73f845d6cf52ff464112a80e181bf1a7f4cec
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-11-11 15:04:49 +0100

    use namespace #2

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -3,8 +3,16 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
+    use super::client;
+
     #[test]
     fn it_works() {
+        // ::client::connect();
+        // is the same as
+        // super::client::connect();
+        // but even better would be
+        client::connect();
+        
         assert_eq!(2 + 2, 4);
     }
 }
