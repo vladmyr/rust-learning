9c018ac550007758a09c4df0eee282c3223cc488
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-12-25 19:16:13 +0100

    error_propagation

---
+++ error_propagation/src/main.rs
@@ -0,0 +1,51 @@
+use std::io;
+use std::io::Read;
+use std::fs;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e)
+    }
+}
+
+fn read_username_from_file_2() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file_3() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file_4() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn main() {
+    let username = read_username_from_file().unwrap();
+    println!("Username is: {}", username);
+
+    let username = read_username_from_file_2().unwrap();
+    println!("Username is: {}", username);
+
+    let username = read_username_from_file_3().unwrap();
+    println!("Username is: {}", username);
+
+    let username = read_username_from_file_4().unwrap();
+    println!("Username is: {}", username);
+}
