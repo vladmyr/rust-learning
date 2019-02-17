fb39258f168e935131b0b61550060fe6b9bd2978
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-02-17 21:47:06 +0100

    closures

---
+++ closures/src/main.rs
@@ -0,0 +1,12 @@
+use closures;
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    closures::generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    )
+}
+
