ab8c665f335e59dc8920d5db624a5976287d3170
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-02-24 14:44:46 +0100

    minigrep -- use iterators over loops + fix unit test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -9,13 +9,20 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
-    }
+  pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+    // first value is the name of the program, skip it
+    args.next();
 
-    let query = args[1].clone();
-    let filename = args[2].clone();
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a query string"),
+    };
+
+    let filename = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a file name")
+    };
+
     let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
     Ok(Config { query, filename, case_sensitive })
@@ -38,28 +45,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-
-  results
+  contents.lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   let query = query.to_lowercase();
-  let mut results = Vec::new();
 
-  for line in contents.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line);
-    }
-  }
-
-  results
+  contents.lines()
+    .filter(|line| line.to_lowercase().contains(&query))
+    .collect()
 }
 
 #[cfg(test)]
@@ -81,10 +77,11 @@ Duct tape.";
     );
   }
 
+  #[test]
   fn case_insenstive() {
     let query = "rUsT";
     let contents = "\
-Rest:
+Rust:
 safe, fast, productive.
 Pick three.
 Trust me.
