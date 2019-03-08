7052b19848a2e6912839432345e2d596bfe413ae
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-03-08 14:21:03 +0100

    blog -- OO way

---
+++ blog/src/lib.rs
@@ -0,0 +1,107 @@
+pub struct Post {
+  state: Option<Box<dyn State>>,
+  content: String
+}
+
+impl Post {
+  pub fn new() -> Post {
+    Post {
+      state: Some(Box::new(Draft {})),
+      content: String::new(),
+    }
+  }
+
+  pub fn add_text(&mut self, text: &str) {
+    self.state.as_ref().unwrap().add_text(&mut self.content, &text);
+  }
+
+  pub fn content(&self) -> &str {
+    self.state.as_ref().unwrap().content(&self)
+  }
+
+  pub fn request_review(&mut self) {
+    if let Some(s) = self.state.take() {
+      self.state = Some(s.request_review())
+    }
+  }
+
+  pub fn approve(&mut self) {
+    if let Some(s) = self.state.take() {
+      self.state = Some(s.approve())
+    }
+  }
+
+  pub fn reject(&mut self) {
+    if let Some(s) = self.state.take() {
+      self.state = Some(s.reject())
+    }
+  }
+}
+
+trait State {
+  // NOTE: self: Box<Self> means the method is only valid when called on a Box
+  // holding the type
+  fn request_review(self: Box<Self>) -> Box<dyn State>;
+  fn approve(self: Box<Self>) -> Box<dyn State>;
+  fn reject(self: Box<Self>) -> Box<dyn State>;
+  fn content<'a>(&self, _post: &'a Post) -> &'a str {
+    ""
+  }
+  fn add_text(&self, _content: &mut String, _text: &str) {}
+}
+
+struct Draft {}
+
+impl State for Draft {
+  fn request_review(self: Box<Self>) -> Box<dyn State> {
+    Box::new(PendingReview {})
+  }
+
+  fn reject(self: Box<Self>) -> Box<dyn State> {
+    self
+  }
+
+  fn approve(self: Box<Self>) -> Box<dyn State> {
+    self
+  }
+
+  fn add_text(&self, content: &mut String, text: &str) {
+    content.push_str(text);
+  }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+  fn request_review(self: Box<Self>) -> Box<dyn State> {
+    self
+  }
+
+  fn reject(self: Box<Self>) -> Box<dyn State> {
+    Box::new(Draft {})
+  }
+
+  fn approve(self: Box<Self>) -> Box<dyn State> {
+    Box::new(Published {})
+  }
+}
+
+struct Published {}
+
+impl State for Published {
+  fn request_review(self: Box<Self>) -> Box<dyn State> {
+    self
+  }
+
+  fn approve(self: Box<Self>) -> Box<dyn State> {
+    self
+  }
+
+  fn reject(self: Box<Self>) -> Box<dyn State> {
+    Box::new(Draft {})
+  }
+
+  fn content<'a>(&self, post: &'a Post) -> &'a str {
+    &post.content
+  }
+}
---
+++ blog/src/main.rs
@@ -0,0 +1,23 @@
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+    
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.add_text("This text goes to void");
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    post.add_text("Another text that goes to void");
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    post.reject();
+    assert_eq!("", post.content());
+}
