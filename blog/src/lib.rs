b71e3601e1bafd7c27bc68bca914f894e4b1cf8c
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-03-08 14:35:37 +0100

    blog -- encoding-state-and-behavior-as-type style refactor

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -1,39 +1,77 @@
 pub struct Post {
-  state: Option<Box<dyn State>>,
-  content: String
+  // state: Option<Box<dyn State>>,
+  content: String,
+}
+
+pub struct DraftPost {
+  content: String,
 }
 
 impl Post {
-  pub fn new() -> Post {
-    Post {
-      state: Some(Box::new(Draft {})),
+  pub fn new() -> DraftPost {
+    DraftPost {
       content: String::new(),
     }
   }
 
-  pub fn add_text(&mut self, text: &str) {
-    self.state.as_ref().unwrap().add_text(&mut self.content, &text);
-  }
-
   pub fn content(&self) -> &str {
-    self.state.as_ref().unwrap().content(&self)
+    &self.content
   }
 
-  pub fn request_review(&mut self) {
-    if let Some(s) = self.state.take() {
-      self.state = Some(s.request_review())
+  // pub fn new() -> Post {
+  //   Post {
+  //     state: Some(Box::new(Draft {})),
+  //     content: String::new(),
+  //   }
+  // }
+
+  // pub fn add_text(&mut self, text: &str) {
+  //   self.state.as_ref().unwrap().add_text(&mut self.content, &text);
+  // }
+
+  // pub fn content(&self) -> &str {
+  //   self.state.as_ref().unwrap().content(&self)
+  // }
+
+  // pub fn request_review(&mut self) {
+  //   if let Some(s) = self.state.take() {
+  //     self.state = Some(s.request_review())
+  //   }
+  // }
+
+  // pub fn approve(&mut self) {
+  //   if let Some(s) = self.state.take() {
+  //     self.state = Some(s.approve())
+  //   }
+  // }
+
+  // pub fn reject(&mut self) {
+  //   if let Some(s) = self.state.take() {
+  //     self.state = Some(s.reject())
+  //   }
+  // }
+}
+
+impl DraftPost {
+  pub fn add_text(&mut self, text: &str) {
+    self.content.push_str(text);
+  }
+
+  pub fn request_review(self) -> PendingReviewPost {
+    PendingReviewPost {
+      content: self.content,
     }
   }
+}
 
-  pub fn approve(&mut self) {
-    if let Some(s) = self.state.take() {
-      self.state = Some(s.approve())
-    }
-  }
+pub struct PendingReviewPost {
+  content: String,
+}
 
-  pub fn reject(&mut self) {
-    if let Some(s) = self.state.take() {
-      self.state = Some(s.reject())
+impl PendingReviewPost {
+  pub fn approve(self) -> Post {
+    Post {
+      content: self.content
     }
   }
 }
--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,23 +1,33 @@
 use blog::Post;
 
 fn main() {
-    let mut post = Post::new();
+    // let mut post = Post::new();
     
+    // post.add_text("I ate a salad for lunch today");
+    // assert_eq!("", post.content());
+
+    // post.request_review();
+    // assert_eq!("", post.content());
+
+    // post.add_text("This text goes to void");
+    // assert_eq!("", post.content());
+
+    // post.approve();
+    // assert_eq!("I ate a salad for lunch today", post.content());
+
+    // post.add_text("Another text that goes to void");
+    // assert_eq!("I ate a salad for lunch today", post.content());
+
+    // post.reject();
+    // assert_eq!("", post.content());
+
+    let mut post = Post::new();
+
     post.add_text("I ate a salad for lunch today");
-    assert_eq!("", post.content());
 
-    post.request_review();
-    assert_eq!("", post.content());
+    let post = post.request_review();
 
-    post.add_text("This text goes to void");
-    assert_eq!("", post.content());
+    let post = post.approve();
 
-    post.approve();
     assert_eq!("I ate a salad for lunch today", post.content());
-
-    post.add_text("Another text that goes to void");
-    assert_eq!("I ate a salad for lunch today", post.content());
-
-    post.reject();
-    assert_eq!("", post.content());
 }
