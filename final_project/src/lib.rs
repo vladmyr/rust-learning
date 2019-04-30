09705ddcfbe3c73d10b3f5095bdf0009544bd422
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-04-30 15:27:51 +0200

    final project -- thread pool implementation

--- final_project/src/lib.rs
+++ final_project/src/lib.rs
@@ -1,8 +1,49 @@
 use std::thread;
 
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+trait FnBox {
+  fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+  fn call_box(self: Box<F>) {
+    (*self)()
+  }
+}
+
+type Job = Box<FnBox + Send + 'static>;
+
+struct Worker {
+  id: usize,
+  thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    let thread = thread::spawn(move || {
+      loop {
+        let job = receiver.lock().unwrap().recv().unwrap();
+
+        println!("Worker {} got a job, executing.", id);
+
+        job.call_box();
+      }
+    });
+
+    Worker {
+      id,
+      thread,
+    }
+  }
+}
+
 pub struct ThreadPool {
-  threads: Vec<thread::JoinHandle<()>>,
-};
+  workers: Vec<Worker>,
+  sender: mpsc::Sender<Job>,
+}
 
 impl ThreadPool {
   /// Create a new ThreadPool
@@ -15,13 +56,18 @@ impl ThreadPool {
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0);
 
-    let mut threads = Vec::with_capasity(size);
+    let (sender, receiver) = mpsc::channel();
+    let receiver = Arc::new(Mutex::new(receiver));
+    let mut workers = Vec::with_capacity(size);
 
-    for _ in 0..size {
-      // create some threads and sotre them in the vectior
+    for id in 0..size {
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
     }
 
-    ThreadPool
+    ThreadPool {
+      workers,
+      sender,
+    }
   }
 
   // TODO: write body with the following signature
@@ -30,6 +76,7 @@ impl ThreadPool {
   pub fn execute<F>(&self, f: F)
     where F: FnOnce() + Send + 'static
   {
-    
+    let job = Box::new(f);
+    self.sender.send(job).unwrap();
   }
 }
