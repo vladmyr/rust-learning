7b9b60dc24e169cf08c67bfdeaf7ec2a27d5f65f
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-05-06 08:31:39 +0200

    types #2

--- prs_types/src/lib.rs
+++ prs_types/src/lib.rs
@@ -16,4 +16,58 @@ mod tests {
     assert_eq!(-1.01f32.floor(), -1.0);
     assert!((-1.0 / std::f32::INFINITY).is_sign_negative());
   }
+
+  // Character type `char` represents a single Unicode character, as a 32-bit 
+  // value
+  #[test]
+  fn char_demo() {
+    assert_eq!('*'.is_alphabetic(), false);
+    assert_eq!('β'.is_alphabetic(), true);
+    assert_eq!('8'.to_digit(10), Some(8));
+    assert_eq!('ಠ'.len_utf8(), 3);
+    assert_eq!(std::char::from_digit(2, 10), Some('2'));
+  }
+
+  // Tuples. Tuples allwo only constants as indices, like t.4, you can't write
+  // t.i or t[i] to get i'th element
+  // zero-tuple() is a "unit type" that is used to indicate there is no
+  // meaningful value to carry
+  #[test]
+  fn tuple_demo() {
+    let text = "I see the eigenvalue in thine eye";
+    let (head, tail) = text.split_at(21);
+    assert_eq!(head, "I see the eigenvalue ");
+    assert_eq!(tail, "in thine eye");
+  }
+
+  // Arrays
+  #[test]
+  fn array_demo() {
+    // demo #1
+    let lazy_caterer: [u32; 6] = [1, 2, 4, 7, 11, 16];
+    let taxonomy = ["Animatlia", "arthropoda", "Insecta"];
+
+    assert_eq!(lazy_caterer[3], 7);
+    assert_eq!(taxonomy.len(), 3);
+
+    // demo #2
+    let mut sieve = [true; 10000];  // all elements set to `true`
+    for i in 2..100 {
+      if sieve[i] {
+        let mut j = i * i;
+        while j < 10000 {
+          sieve[j] = false;
+          j += i;
+        }
+      }
+    }
+
+    assert!(sieve[211]);
+    assert!(!sieve[9876]);
+
+    // demo #3
+    let mut chaos = [3, 5, 4, 1, 2];
+    chaos.sort();
+    assert_eq!(chaos, [1, 2, 3, 4, 5]);
+  }
 }
